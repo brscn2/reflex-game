f61f99fbe231bbecbe51e44ad795c707d027e837
brscn2 <brscn55@yahoo.com>
2022-01-27 19:02:24 +0300

    updated version

--- public/js/game.js
+++ public/js/game.js
@@ -1,4 +1,10 @@
 $(document).ready(() => {
+    $("#game-button").on("keydown", function (e) {
+        if (e.keyCode === 13 || e.keyCode === 32) {
+            e.preventDefault();
+        }
+    });
+
     let gameInterval;
     let intervalTime = 6000;
     let score = -1;
@@ -21,6 +27,13 @@ $(document).ready(() => {
         return buttonSize;
     }
 
+    const randomButtonColor = () => {
+        let red = Math.floor(Math.random() * 256);
+        let green = Math.floor(Math.random() * 256);
+        let blue = Math.floor(Math.random() * 256);
+        return `rgb(${red},${green},${blue})`;
+    }
+
     const offsetButton = () => {
         $("#game-button").css("left", `${randomLeftCoordinate()}%`);
         $("#game-button").css("top", `${randomTopCoordinate()}%`);
@@ -32,6 +45,10 @@ $(document).ready(() => {
         $("#game-screen").css("text-align", "center");
 
         $("#game-button").text("Start!");
+
+        $("#game-button").addClass("btn-warning");
+
+        $("#game-button").css("background-color", "");
         $("#game-button").css("padding", "6% 5%");
         $("#game-button").css("left", "0");
         $("#game-button").css("top", "0");
@@ -44,11 +61,17 @@ $(document).ready(() => {
         $("#game-screen").css("text-align", "left");
 
         $("#game-button").text("Here!");
+
+        $("#game-button").removeClass("btn-warning");
+
         let size = randomButtonSize();
         $("#game-button").css("padding", `${size + 1}% ${size}%`);
 
+        $("#game-button").css("background-color", `${randomButtonColor()}`);
+
         clearInterval(gameInterval);
 
+        $("#game-button").css("transition", "0.1s ease-in-out");
         offsetButton();
 
         score++;

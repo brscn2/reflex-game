e35950fa9c52f221cc6e76fb19ae20e0b9eba69f
brscn2 <brscn55@yahoo.com>
2022-01-27 15:17:06 +0300

    update

--- public/js/game.js
+++ public/js/game.js
@@ -1,6 +1,10 @@
 $(document).ready(() => {
     let gameInterval;
+    let intervalTime = 6000;
     let score = -1;
+
+    $("#game-button").text("Start!");
+    $("#game-button").css("padding", "6% 5%");
 
     const randomLeftCoordinate = () => {
         let leftOffset = Math.floor(Math.random() * 90);
@@ -8,8 +12,13 @@ $(document).ready(() => {
     }
 
     const randomTopCoordinate = () => {
-        let topOffset = Math.floor(Math.random() * 90);
+        let topOffset = Math.floor(Math.random() * 80);
         return topOffset;
+    }
+
+    const randomButtonSize = () => {
+        let buttonSize = Math.floor(Math.random() * 5 + 1);
+        return buttonSize;
     }
 
     const offsetButton = () => {
@@ -22,6 +31,8 @@ $(document).ready(() => {
 
         $("#game-screen").css("text-align", "center");
 
+        $("#game-button").text("Start!");
+        $("#game-button").css("padding", "6% 5%");
         $("#game-button").css("left", "0");
         $("#game-button").css("top", "0");
 
@@ -32,6 +43,10 @@ $(document).ready(() => {
     $("#game-button").click(() => {
         $("#game-screen").css("text-align", "left");
 
+        $("#game-button").text("Here!");
+        let size = randomButtonSize();
+        $("#game-button").css("padding", `${size + 1}% ${size}%`);
+
         clearInterval(gameInterval);
 
         offsetButton();
@@ -39,9 +54,15 @@ $(document).ready(() => {
         score++;
         $("#score").text(score);
 
+        if (score < 50) {
+            intervalTime = 6000 - 1000 * (score / 10);
+        } else {
+            intervalTime = 750;
+        }
+
         gameInterval = setInterval(() => {
             gameOver();
             clearInterval(gameInterval);
-        }, 1000)
+        }, intervalTime);
     })
 })
